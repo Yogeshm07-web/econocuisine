ab8b2193fae8965e66ec8bf841ee31e1bc183a5c
Koovalesh Gaurish Rambojun <koovaleshgau@gmail.com>
2024-02-10 14:10:17 +0400

    basket 2.0

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,34 @@
 class ProductController < ApplicationController
+
+def index
+  @products=Product.all
+end
+
+def create
+  @product = Product.new(product_params)
+  @product.save
+  redirect_to product_path(@products)
+end
+
+def show
+  @product = Product.find(params[:id])
+end
+
+
+
+def update
+  @product = Product.find(params[:id])
+  @product.update(product_params)
+  redirect_to product_path(@product)
+end
+
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to product_path, status: :see_other
+end
+
+def product_params
+  params.require(:product).permit(:id, :name, :description,:category,:price,:stock)
+end
 end
