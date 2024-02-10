8aa7610bb03ebfec77b679bbf55a9792b4e28fcc
Koovalesh Gaurish Rambojun <koovaleshgau@gmail.com>
2024-02-10 12:09:02 +0400

    basket

---
+++ db/migrate/20240210080328_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.integer :id
+      t.string :name
+      t.string :description
+      t.string :category
+      t.integer :price
+      t.integer :stock
+
+      t.timestamps
+    end
+  end
+end
