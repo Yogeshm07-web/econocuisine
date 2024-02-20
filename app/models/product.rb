1b3db76f5fca26cc44e485c503063595031e79e7
yogesh munsah <yogeshmunsah07@gmail.com>
2024-02-20 18:41:43 +0400

    lewagonsupermarket

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   has_many :order_items
   has_one_attached :image
-
-  # Ensure that the quantity unit and abbreviation are present
-  validates :quantity_unit, :quantity_unit_abbreviation, presence: true
-
+  belongs_to :category
+  has_many :basket_items
+  # Ensure that the necessary attributes are present
+  validates :name, :description, :category, :price, :stock, :quantity_unit, presence: true
+  scope :featured, -> { where(featured: true) }
   # No need to define custom methods for attribute access
   # You can directly access attributes using ActiveRecord's attribute methods
 end
