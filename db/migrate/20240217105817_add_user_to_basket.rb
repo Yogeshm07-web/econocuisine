0cd2fcc48ecb3b4825ddbc2bd2f0021205f0b574
Koovalesh Gaurish Rambojun <koovaleshgau@gmail.com>
2024-02-20 19:48:44 +0400

    modified

--- db/migrate/20240217105817_add_user_to_basket.rb
+++ db/migrate/20240217105817_add_user_to_basket.rb
@@ -1,4 +1,4 @@
-class AddUsertoBasket < ActiveRecord::Migration[7.1]
+class AddUserToBasket < ActiveRecord::Migration[7.1]
   def change
     add_reference :baskets, :user, null: false, foreign_key: true
   end
