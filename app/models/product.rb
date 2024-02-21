f8ee1dac125c19f6c93223fc5de5e7ccfc973318
yogesh munsah <yogeshmunsah07@gmail.com>
2024-02-21 13:54:01 +0400

    supermarket1

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   has_many :basket_items
+  attr_accessor :quantity_bought
   # Ensure that the necessary attributes are present
   validates :name, :description, :category, :price, :stock, :quantity_unit, presence: true
   scope :featured, -> { where(featured: true) }
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  # Remove the line for 'budgets/index' and replace it with the route to 'pages#budget'
   resources :budgets, only: [:index]
-  # Keep other routes as they are
   devise_for :users
   resources :inventories
   resources :baskets, only: [:index, :show, :create, :update, :destroy]
   resources :products
-  post 'add_to_basket/:id', to: 'baskets#add_to_basket', as: 'add_to_basket'
   get 'le_wagon_supermarket', to: 'products#le_wagon_supermarket'
   root to: "pages#home"
   get '/signin', to: 'sessions#new'
   get '/signup', to: 'users#new'
+
+  post '/baskets/add_to_basket/:id', to: 'baskets#add_to_basket', as: 'add_to_basket'
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
