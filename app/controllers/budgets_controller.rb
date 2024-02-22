2e0178cbd4134e8fae9f4c64bade692261820dbb
yogesh munsah <yogeshmunsah07@gmail.com>
2024-02-22 18:23:08 +0400

    supermarket5

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,4 +2,19 @@ class BudgetsController < ApplicationController
   def index
     @budgets = Budget.all
   end
+
+  def update
+    @budget = Budget.find(params[:id])
+    if @budget.update(budget_params)
+      redirect_to budgets_path, notice: 'Budget was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def budget_params
+    params.require(:budget).permit(:name, :amount) # Adjust permitted parameters as per your model
+  end
 end
