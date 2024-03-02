00a904784b44cbe743be615d3606d0b935e1909c
y''m <yogeshmunsah07@gmail.com>
2024-03-02 14:13:29 +0400

    heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :budgets, only: [:index]
   devise_for :users
+  delete "basket_items/:id", to: "basket_items#destroy", as: :basket_item_delete
   resources :inventories
   resources :baskets, only: [:index, :show, :create, :update, :destroy] do
     post 'add_to_basket', on: :member
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   resources :incomes
   resources :expenses
   resources :basket_items, only: [:create]
-  delete "basket_items/:id", to: "basket_items#destroy", as: :basket_item_delete
 
 
   get 'le_wagon_supermarket', to: 'products#le_wagon_supermarket'
