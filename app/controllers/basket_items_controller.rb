9bb0dc792c759696af6ada1dbcbe4e7fbff50451
y''m <yogeshmunsah07@gmail.com>
2024-03-05 14:38:45 +0400

    gjgjg12

--- app/controllers/basket_items_controller.rb
+++ app/controllers/basket_items_controller.rb
@@ -1,25 +1,6 @@
 class BasketItemsController < ApplicationController
   before_action :set_basket_item, only: [:destroy]
 
-  def create
-    @basket_item = current_user.basket_items.build(basket_item_params)
-    if @basket_item.save
-      redirect_to baskets_path, notice: 'Item added to basket.'
-    else
-      flash[:alert] = 'Failed to add item to basket.'
-      render :new  # Rendering the new action/view for creating basket items again
-    end
-  end
-  
-  def show
-    @basket_item = BasketItem.find(params[:id])
-      respond_to do |format|
-      format.html # show.html.erb
-      
-      format.json { render json: @basket_item }
-    end
-  end
-
   def destroy
     @basket = Basket.find(params[:basket_id])
     @basket_item = @basket.basket_items.find(params[:id])
