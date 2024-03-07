b444d23c501a475384e1a85819fc738dfd9393d2
y''m <yogeshmunsah07@gmail.com>
2024-03-07 16:51:42 +0400

    reviewbackendfix

--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -1,5 +1,11 @@
 class BasketItem < ApplicationRecord
-  belongs_to :basket
   belongs_to :product
-  attribute :price, :decimal
+  belongs_to :basket
+
+  # Define the total_price method to calculate the total price of the basket item
+  def total_price
+    return 0 if quantity.nil? || price.nil?
+
+    quantity * price
+  end
 end
