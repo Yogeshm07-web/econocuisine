5d4709b5a3308b7f5a606f1eff2ff013b1fb6264
y''m <yogeshmunsah07@gmail.com>
2024-03-09 09:35:49 +0400

    done

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,5 @@ class Product < ApplicationRecord
   # Ensure that the necessary attributes are present
   validates :name, :description, :category, :price, :stock, :quantity_unit, presence: true
   scope :featured, -> { where(featured: true) }
-  # No need to define custom methods for attribute access
-  # You can directly access attributes using ActiveRecord's attribute methods
+  
 end
